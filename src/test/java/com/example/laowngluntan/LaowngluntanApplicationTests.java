c33591d27f41df90757c78f501d64a0f20e5efa0
laowanghenlan <1985680256@qq.com>
2019-10-02 13:37:46 +0800

    init reap

---
+++ src/test/java/com/example/laowngluntan/LaowngluntanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.laowngluntan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LaowngluntanApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
