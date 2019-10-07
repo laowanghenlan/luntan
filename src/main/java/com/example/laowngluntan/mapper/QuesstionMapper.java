1c33e6036dbda68e90623399b21cef32f223ef99
laowanghenlan <1985680256@qq.com>
2019-10-07 21:38:23 +0800

    wentifenye

--- src/main/java/com/example/laowngluntan/mapper/QuesstionMapper.java
+++ src/main/java/com/example/laowngluntan/mapper/QuesstionMapper.java
@@ -12,10 +12,16 @@ import java.util.List;
 public interface QuesstionMapper {
     @Insert("insert into question (title,description,gmt_create,gmt_modified,creator,tag) values (#{title},#{description},#{gmtCreate},#{gmtModified},#{creator},#{tag})")
     public void create(Question question);
+
      @Select("select * from question limit #{offset},#{size}")
      List<Question> list(@Param(value ="offset") Integer offset , @Param(value ="size") Integer size);
 
-
-     @Select("select  count(1) from  QUESTION;")
+     @Select("select  count(1) from  question")
      Integer count();
+
+     @Select("select * from question where creator = #{userId} limit #{offset},#{size}")
+    List<Question> listByUserId(@Param("userId") Integer userId, @Param(value ="offset") Integer offset , @Param(value ="size")Integer size);
+
+    @Select("select  count(1) from  question where creator = #{userId}")
+    Integer countByUserId(@Param("userId") Integer userId);
 }
