6160fac89f522f00a8cf26409003edaf42123f8e
laowanghenlan <1985680256@qq.com>
2019-10-13 16:18:39 +0800

    二级评论页面设计完成

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -1,3 +1,6 @@
+/*
+* 提交回复
+* */
 function post() {
    var questionId = $("#question_id").val();
     var content = $("#comment_content").val();
@@ -32,3 +35,26 @@ function post() {
         dataType: "json"
     });
 }
+/*
+* 二级评论
+* */
+function collapseComments(e) {
+    var id = e.getAttribute("data-id");
+    var comments = $("#comment-"+id);
+    //获取二级评论的展开状态
+    var collapse = e.getAttribute("data-collapse");
+    if (collapse){
+        //折叠二级评论
+        comments.removeClass("in");
+        e.removeAttribute("data-collapse");
+        e.classList.remove("active");
+    }else {
+        //展开二级评论
+        comments.addClass("in");
+        //标记二级评论展开状态
+        e.setAttribute("data-collapse","in");
+        e.classList.add("active");
+    }
+    }
+
+
